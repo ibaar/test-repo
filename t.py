2c027e1ec65471fe22026fa6f5db2dcfd38a6b40
Ibar <92300382+ibar2@users.noreply.github.com>
2022-04-15 15:20:38 +0300

    why are you running

--- t.py
+++ t.py
@@ -2,10 +2,11 @@ import subprocess
 
 
 def main(c=4):
+    ad = input('hello input any thing: ')
     subprocess.run(['python', '-V'])
     a = 10
     print(f'{a*c=}')
-
+    print(ad)
     return 'Nothing'
 
 
