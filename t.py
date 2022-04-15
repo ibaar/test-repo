956503afa6ae51e92cd65509070ed8f13d1ff4b1
Ibar <92300382+ibar2@users.noreply.github.com>
2022-04-15 15:53:39 +0300

    a for co

--- t.py
+++ t.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 def main(c=4):
-    ad = input('hello input only integers and floats: ')
+    ad = input('hello input only strings and lists: ')
     subprocess.run(['python', '-V'])
     a = 10
     print(f'{a*c=}')
