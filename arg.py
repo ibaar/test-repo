facdb38bf2baac635d28a7e8d9479b457e5aa790
Ibar <92300382+ibar2@users.noreply.github.com>
2022-04-19 19:13:42 +0300

    :

--- arg.py
+++ arg.py
@@ -1,5 +1,5 @@
 import argparse
-
+print("there is alot of thing that is wrong with this shit")
 def main() -> None:
     a =argparse.ArgumentParser(description="this is a test app",)
     a.add_argument("-a","--add",help= 'this takes a string and print it')
